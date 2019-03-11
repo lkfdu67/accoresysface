6389dae32e70be22f0cea7dda8fe99e03453c450
zhanghuashine <zhanghuashine@gmail.com>
2019-03-11 18:02:32 +0800

    update net.cpp

--- src/net.cpp
+++ src/net.cpp
@@ -4,17 +4,13 @@
 #include "../inc/net.hpp"
 using namespace caffe;
 
-Net::Net(const string &trained_file) {
+template <typename Dtype>
+Net<typename Dtype>::Net(const string &trained_file) {
     Init(trained_file);
 }
 
-///均值处理放到外层去做
-//Net::Net(const string &trained_file, const string &mean_file) {
-//    Init(trained_file);
-//    set_mean(mean_file);
-//}
-
-void Net::Init(const string &trained_file) {
+template <typename Dtype>
+void Net<typename Dtype>::Init(const string &trained_file) {
     bool success = true;
     NetParameter* param;
     if(!ReadNetParamsFromBinaryFile(trained_file, param))
@@ -23,30 +19,51 @@ void Net::Init(const string &trained_file) {
     }else{
         cout<<"Reading net parameters is failed!"<<endl;
         success = false;
-        assert(success);
+        assert(success);  // 调试
     }
     if(!UpdateNet(param)){
         cout<<"Updating net parameters is successful!"<<endl;
     }else{
         cout<<"Updating net parameters is failed!"<<endl;
         success = false
-        assert(success);
+        assert(success);  //调试
     }
 }
 
-int Net::ReadNetParamsFromBinaryFile(const string &trained_file, NetParameter *param) {
-    // 判断文件存在与否
-    // 。。。
+template <typename Dtype>
+int Net<typename Dtype>::ReadNetParamsFromBinaryFile(const string &trained_file, caffe::NetParameter& netparam) {
+    /// 解析正确返回： 0,否则返回： 1
+    ifstream caffemodel(trained_file, ifstream::in | ifstream::binary);
+
+    if (&caffemodel == NULL){
+        cout << "The ptr of caffemodel is NULL" << endl;
+        return 1;
+    }
+
+    if (!caffemodel.is_open()){
+        cout << "Can not open model" << endl;
+        return 1;
+    }
+
+    // 解析参数，存入netparam成员变量中
+    bool flag = netparam.ParseFromIstream(&caffemodel);
+
+    caffemodel.close();
+
+    if(!flag){
+        cout<<"Parsing net_parameter from io stream error!";
+        return 1;
+    }
+    return 0;
+}
+
+template <typename Dtype>
+int Net<typename Dtype>::UpdateNet(NetParameter* param){
 
     return 0;
 }
 
-int Net::UpdateNet(NetParameter* param){
-
-
-    return 0;
-}
-
-void Net::Forward(const caffe::Blob<Dtype>& input_data, const string &begin, const string &end){
+template <typename Dtype>
+void Net<typename Dtype>::Forward(const caffe::Blob<Dtype>& input_data, const string &begin, const string &end){
     ;
 }
