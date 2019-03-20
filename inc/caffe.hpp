85dd25bb3e822300e3ad98eb327e4fdd349eb2fd
zhanghuashine <zhanghuashine@gmail.com>
2019-03-20 09:47:53 +0800

    add some update:net.hpp net.cpp

---
+++ inc/caffe.hpp
@@ -0,0 +1,13 @@
+//
+// Created by hua on 19-3-19.
+//
+
+#ifndef LOADPARAM_COMMON_HPP
+#define LOADPARAM_COMMON_HPP
+
+#include "layer.hpp"
+#include "layer/convolution.hpp"
+#include "layer/pooling.hpp"
+#include "layer/relu.hpp"
+
+#endif //LOADPARAM_COMMON_HPP
--- src/net.cpp
+++ src/net.cpp
@@ -2,39 +2,54 @@
 // Created by hua on 19-3-7.
 //
 #include "net.hpp"
+#include "caffe.hpp"
 using namespace caffe;
 
-Net::Net(const string& model_file, const string& trained_file) {
-    NetParameter param;
-    ReadNetParamsFromTextFile(model_file, param);
-    Init(param);  // 初始化网络模型
-}
+//Net::Net(const string& model_file, const string& trained_file) {
+//    NetParameter param;
+//    ReadNetParamsFromTextFile(model_file, param);
+//    Init(param);  // 初始化网络模型
+//}
 
 void Net::Init(const NetParameter& in_param){
+
+    const int layers_size = in_param.layer_size();
+    bottom_vecs_.resize(layers_size);
+    top_vecs_.resize(layers_size);
+
     shared_ptr<Layer> layer(NULL);
     // 循环遍历每一层，进行初始化
     for(int layer_id=0; layer_id<in_param.layer_size(); ++layer_id){
-        // Setup layer.
-        const LayerParameter& layer_param = param.layer(layer_id);
-        if("convolution" == layer_param.layer_type)
+
+        const LayerParameter& layer_param = in_param.layer(layer_id);
+        pair<string, shared_ptr<Blob>> tmp;
+        for(int bottom_id=0; bottom_id<layer_param.bottom_size(); ++bottom_id){
+            tmp = make_pair(layer_param.bottom(bottom_id), NULL);
+            bottom_vecs_[layer_id].push_back(tmp);
+        }
+
+        for(int top_id=0; top_id<layer_param.top_size(); ++top_id){
+            tmp = make_pair(layer_param.top(top_id), NULL);
+            top_vecs_[layer_id].push_back(tmp);
+        }
+
+        if("convolution" == layer_param.type())
         {
-            Convolution* conv = NULL;
-            layer.reset(conv);
+            layer.reset(new Convolution);
 
         }
-        else if ("pooling" == layer_param.layer_type)
+        else if ("pooling" == layer_param.type())
         {
-            Pooling* pool = NULL;
-            layer.reset(pool);
+            layer.reset(new Pooling);
 
         }
-        else if("relu" == layer_param.layer_type)
+        else if("relu" == layer_param.type())
         {
-            Relu* relu = NULL;
-            layer.reset(relu);
+            layer.reset(new Relu);
         }
-        layer.Init();  // 打印消息
-        layers_[layer_param.name()].reset(layer);
+        //SetUp中传一个输入尺寸，vector<int>: n, c, h, w?
+        layer.SetUp(layer_param, bottom_vecs_[layer_id], top_id_vecs_[layer_id]);  // 打印消息
+
+        layers_[layer_param.name()] = layer;
     }
 }
-
