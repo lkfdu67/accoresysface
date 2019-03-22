53d3c6e3ed51478c3e840a473eb506b8331cf136
zhanghuashine <zhanghuashine@gmail.com>
2019-03-22 18:10:01 +0800

    add blob update...

--- inc/blob.hpp
+++ inc/blob.hpp
@@ -5,13 +5,115 @@
 #ifndef LOADPARAM_BLOB_HPP
 #define LOADPARAM_BLOB_HPP
 
+#include <armadillo>
+using namespace arma;
+using namespace std;
+
+
 namespace caffe{
 
 class Blob{
-    ;
-};
+public:
+	Blob(): data_(), shape_() {}
 
-}
+	Blob(const int num, const int channels, const int height, const int width);
+
+	explicit Blob(const vector<int>& shape);
+
+	explicit Blob(const vector<cube>& cubes);
+
+	Blob(const Blob&);
+
+	Blob& operator=(const Blob&);
+
+	virtual ~Blob() {}
+
+
+	///
+	inline const vector<int>& shape() const {
+		return shape_;
+	}
+
+	inline int shape(int index) const {
+		if (index < 0 || index >= 4) {
+			return 1;
+		}
+		return shape_[index];
+	}
+
+	inline int num() const {
+		return data_.size();
+	}
+
+	inline int channels() const {
+		return data_.size() > 0 ? data_[0].n_slices : 0;
+	}
+
+	inline int height() const {
+		return data_.size() > 0 ? data_[0].n_rows : 0;
+	}
+
+	inline int width() const {
+		return data_.size() > 0 ? data_[0].n_cols : 0;
+	}
+
+	inline const vector<cube>& data() const {
+		return data_;
+	}
+
+	void print_data() const;
+
+
+	///
+	Blob sub_blob(const int channel_start) const;
+
+	Blob sub_blob(const int channel_start, const int channel_end) const;
+
+	Blob sub_blob(const int channel_start, const int height_start, const int width_start) const;
+
+	Blob sub_blob(const int channel_start, const int channel_end, const int height_start, 
+		const int height_end, const int width_start, const int width_end) const;
+
+	Blob operator()(vector<int> channel) const;
+
+	Blob operator()(vector<int> channel, vector<int> height, vector<int> width) const;
+
+	vector<double> operator[](const int index) const;
+
+
+	///
+	vector<double> sum() const;
+
+	void scale(const double scale_factor);
+
+	Blob operator+(const Blob& other) const;
+
+	Blob& operator+=(const Blob& other);
+
+	Blob operator*(const Blob& other) const;
+
+	Blob& operator*=(const Blob& other);
+
+	//friend Blob& operator*(const Blob& other, const double scale_factor);
+
+	Blob operator*(const double scale_factor) const;
+
+	Blob& operator*=(const double scale_factor);
+
+	template<typename functor>
+	Blob& elem_wise_op(functor lambda_function);
+
+private:
+	//int num_;
+	//int channels_;
+	//int height_;
+	//int width_;
+	vector<cube> data_;
+	vector<int> shape_;
+
+};	//class Blob
+
+}	//namespace caffe
 
 
 #endif //LOADPARAM_BLOB_HPP
--- src/blob.cpp
+++ src/blob.cpp
@@ -2,3 +2,235 @@
 // Created by hua on 19-3-14.
 //
 #include "blob.hpp"
+
+namespace caffe {
+
+Blob::Blob(const int num, const int channels, const int height, const int width)
+{
+	this->shape_.push_back(num);
+	this->shape_.push_back(channels);
+	this->shape_.push_back(height);
+	this->shape_.push_back(width);
+	for (int i = 0; i < num; i++) {
+		cube c(height, width, channels);
+		c.fill(0);
+		this->data_.push_back(c);
+	}
+}
+
+Blob::Blob(const vector<int>& shape)
+{
+	this->shape_ = shape;
+	for (int i = 0; i < shape[0]; i++) {
+		cube c(shape[2], shape[3], shape[1]);
+		c.fill(0);
+		this->data_.push_back(c);
+	}
+}
+
+Blob::Blob(const vector<cube>& cubes)
+{
+	for (auto cube : cubes) {
+		this->data_.push_back(cube);
+	}
+	this->shape_.push_back(cubes.size());
+	this->shape_.push_back(cubes[0].n_slices);
+	this->shape_.push_back(cubes[0].n_rows);
+	this->shape_.push_back(cubes[0].n_cols);
+}
+
+Blob::Blob(const Blob& other)
+{
+	this->data_ = other.data_;
+	this->shape_ = other.shape_;
+}
+
+Blob& Blob::operator=(const Blob& other)
+{
+	if (this != &other) {
+		if (!this->data_.empty()) {
+			this->data_.clear();
+		}
+		this->data_ = other.data_;
+
+		if (!this->shape_.empty()) {
+			this->shape_.clear();
+		}
+		this->shape_ = other.shape_;
+	}
+	return *this;
+}
+
+void Blob::print_data() const 
+{
+	int i = 1;
+	for (auto d : data_) {
+		d.print("Batch " + to_string(i++) + " :");
+	}
+}
+
+Blob Blob::sub_blob(const int channel_start) const
+{
+	Blob b;
+	for (auto d : data_) {
+		_ASSERT(channel_start < d.n_slices);
+		b.data_.push_back(d.slices(channel_start, d.n_slices));
+	}
+	return b;
+}
+
+Blob Blob::sub_blob(const int channel_start, const int channel_end) const
+{
+	//_ASSERT(channel_start <= channel_end);
+	Blob b;
+	for (auto d : data_) {
+		//_ASSERT(channel_end < d.n_slices);
+		b.data_.push_back(d.slices(channel_start, channel_end));
+	}
+	return b;
+}
+
+Blob Blob::sub_blob(const int height_start, const int width_start, const int channel_start) const
+{	
+	Blob b;
+	for (auto d : data_) {
+		//_ASSERT(height_start < d.n_rows && width_start < d.n_cols && channel_start < d.n_slices);
+		b.data_.push_back(d.subcube(height_start, height_start, channel_start, d.n_rows, d.n_cols, d.n_slices));
+	}
+	return b;
+}
+
+Blob Blob::sub_blob(const int height_start, const int height_end, const int width_start,
+	const int width_end, const int channel_start, const int channel_end) const
+{
+	//_ASSERT(height_start < height_end && width_start < width_end && channel_start <= channel_end);
+	Blob b;
+	for (auto d : data_) {		
+		//_ASSERT(height_end < d.n_rows && width_end < d.n_cols && channel_end < d.n_slices);
+		b.data_.push_back(d.subcube(height_start, height_start, channel_start, height_end, width_end, channel_end));
+	}
+	return b;
+}
+
+Blob Blob::operator()(vector<int> channel) const
+{
+	_ASSERT(channel.size() == 2);
+	Blob b;
+	for (auto d : data_) {
+		b.data_.push_back(d.slices(channel[0], channel[1]));
+	}
+	return b;
+}
+
+Blob Blob::operator()(vector<int> channel, vector<int> height, vector<int> width) const
+{	
+	_ASSERT(height.size() == 2 && width.size() == 2 && channel.size() == 2);
+	//_ASSERT(height[0] < height[1] && width[0] < width[1]);
+	Blob b;
+	for (auto d : data_) {
+		//_ASSERT(height[1] < d.n_rows && width[1] < d.n_cols);
+		b.data_.push_back(d.subcube(height[0], width[0], channel[0], height[1], width[1], channel[1]));
+	}
+	return b;
+}
+
+vector<double> Blob::operator[](const int index) const
+{
+	vector<double> elem;
+	for (auto d : data_) {
+		elem.push_back(d(index));
+	}
+	return elem;
+}
+
+vector<double> Blob::sum() const
+{
+	vector<double> sum;
+	for (auto d : data_) {
+		sum.push_back(accu(d));
+	}
+	return sum;
+}
+
+void Blob::scale(const double scale_factor) {
+	for (auto &d : data_) {
+		d = scale_factor * d;
+	}
+}
+
+Blob Blob::operator+(const Blob& other) const
+{
+	_ASSERT(this->data_.size() == other.data_.size());
+	int i = 0;
+	Blob b;
+	for (auto d : data_) {
+		b.data_.push_back(d + other.data_[i++]);
+	}
+	return b;
+}
+
+Blob& Blob::operator+=(const Blob& other) 
+{
+	_ASSERT(this->data_.size() == other.data_.size());
+	int i = 0;
+	for (auto &d : data_) {
+		d += other.data_[i++];
+	}
+	return *this;
+}
+
+Blob Blob::operator*(const Blob& other) const
+{
+	_ASSERT(this->data_.size() == other.data_.size());
+	int i = 0;
+	Blob b;
+	for (auto d : data_) {
+		cube c(d.n_rows, d.n_cols, d.n_slices);
+		for (int j = 0; j < d.n_slices; j++) {
+			c.slice(j) = d.slice(j) * other.data_[i].slice(j);
+		}
+		b.data_.push_back(c);
+		i++;
+	}
+	return b;
+}
+
+Blob& Blob::operator*=(const Blob& other) 
+{
+	_ASSERT(this->data_.size() == other.data_.size());
+	int i = 0;
+	for (auto &d : data_) {
+		for (int j = 0; j < d.n_slices; j++) {
+			d.slice(j) *= other.data_[i].slice(j);
+		}
+		i++;
+	}
+	return *this;
+}
+
+Blob Blob::operator*(const double scale_factor) const
+{
+	Blob b;
+	for (auto d : data_) {
+		b.data_.push_back(scale_factor * d);
+	}
+	return b;
+}
+
+Blob& Blob::operator*=(const double scale_factor)
+{
+	for (auto &d : data_) {
+		d = scale_factor * d;
+	}
+	return *this;
+}
+
+template<typename functor>
+Blob& Blob::elem_wise_op(functor lambda_function)
+{
+	for (auto &d : data_) {
+		d.transform(lambda_function);
+	}
+}
+
+}	//namespace caffe
