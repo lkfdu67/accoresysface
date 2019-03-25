5b83eeadf813f352a5a9dac015497f7164a9df5e
zhanghuashine <zhanghuashine@gmail.com>
2019-03-25 17:23:30 +0800

    add net update...

--- inc/net.hpp
+++ inc/net.hpp
@@ -24,6 +24,8 @@ using std::string;
 using std::pair;
 using std::shared_ptr;
 using std::set;
+using std::cout;
+using std::endl;
 
 namespace caffe{
 
@@ -36,8 +38,16 @@ public:
     /// @brief 使用NetParameter初始化网络
     void Init(const NetParameter& param);
 
+    /// @brief 使用NetParameter初始化网络
+    void CopyTrainedParams(const string& trained_file);
+
     /// @brief 前向计算，输入：处理数据、计算开始的位置+结束的位置，返回：结束位置对应的运算结果
     const Blob* Forward(const Blob& input_data, const string& begin, const string& end);
+
+    /// @brief 得到前向运算结果
+    inline const vector<Blob*>& output_blobs() const {
+        return net_output_blobs_;
+    }
 
 protected:
     ;
@@ -46,6 +56,7 @@ private:
     // Traversal layer's parameter layer by layer from layer id.
     vector<std::shared_ptr<Layer> > layers_;
     map<string, int> layer_name_id_; // 获取layers_中指定层对象
+    vector<string> layer_names_;
 
     // bottom
     vector<vector<Blob*> > bottom_vecs_;
@@ -55,10 +66,12 @@ private:
     vector<vector<Blob*> > top_vecs_;
     vector<vector<int> > top_id_vecs_;    // 和blob_对应(考虑每一层可能有多个top)，除了训练以外，貌似用不到
 
-    //存储了每一层输出结果, id:[0~layer.size())?
-    vector<shared_ptr<Blob>> blobs_;
-    vector<string> blob_names_;
-    // map<string, int> blob_names_index_;
+    //存储了每一层输出结果, id:[0~layers.size())?
+    vector<shared_ptr<Blob> > blobs_;
+    vector<string> blob_names_;  // bottom、top 通过名字访问对应内存
+
+    //net output
+    vector<Blob*> net_output_blobs_;
 
 };
 
