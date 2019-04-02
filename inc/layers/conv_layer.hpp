d661a9a5eac5efe8e9e3582af5841124b650a748
lkfdu67 <lkfdu67_wy@163.com>
2019-04-02 09:47:07 +0800

    add shared_ptr

--- inc/layers/conv_layer.hpp
+++ inc/layers/conv_layer.hpp
@@ -6,6 +6,8 @@
 #define LOADPARAM_CONV_LAYER_HPP
 
 #include "layer.hpp"
+#include "blob.hpp"
+#include <memory>
 
 using std::vector;
 using std::shared_ptr;
@@ -19,11 +21,26 @@ namespace caffe{
     public:
         ConvLayer(){}
         ~ConvLayer(){}
-        void SetUp(const LayerParameter& param, const vector<Blob<double>*>& bottom, vector<Blob<double>*>& top);
-        void Forward(const vector<Blob<double>*>& bottom, vector<Blob<double>*>& top);
+        void SetUp(const LayerParameter& param, const vector<shared_ptr<Blob<double>>>& bottom, vector<shared_ptr<Blob<double>>>& top);
+        void Forward(const vector<shared_ptr<Blob<double>>>& bottom, vector<shared_ptr<Blob<double>>>& top);
 
     private:
-        LayerParameter layer_param_;
+        string layer_type_;
+        string layer_name_;
+        vector<string> bottom_names_;
+        vector<string> top_names_;
+
+        int pad_w_;
+        int pad_h_;
+        int stride_w_;
+        int stride_h_;
+        int kernel_w_;
+        int kernel_h_;
+
+        int num_output_;
+        int num_channel_;
+        bool bias_term_;
+
         vector<int> in_shape_;
         vector<int> out_shape_;
         void calc_shape_(const vector<int>& in_shape, vector<int>& out_shape);
