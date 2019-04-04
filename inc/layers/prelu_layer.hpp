b6fc2f02834d965fa2ab30a529e2d49f8c98eb9a
lkfdu67 <lkfdu67_wy@163.com>
2019-04-04 14:12:49 +0800

    update prelu

---
+++ inc/layers/prelu_layer.hpp
@@ -0,0 +1,37 @@
+//
+// Created by liukai on 19-4-4.
+//
+
+#ifndef LOADPARAM_PRELU_LAYER_HPP
+#define LOADPARAM_PRELU_LAYER_HPP
+
+#include "layer.hpp"
+
+using std::vector;
+using std::shared_ptr;
+using std::string;
+using std::pair;
+
+namespace caffe{
+
+    class PReluLayer : public Layer
+    {
+    public:
+        PReluLayer(){}
+        ~PReluLayer(){}
+        void SetUp(const LayerParameter& param, const vector<Blob<double>* >& bottom, vector<Blob<double>* >& top);
+        void Forward(const vector<Blob<double>* >& bottom, vector<Blob<double>* >& top);
+
+    private:
+//        string layer_type_;
+//        string layer_name_;
+//        vector<string> bottom_names_;
+//        vector<string> top_names_;
+
+        vector<int> in_shape_;
+//        vector<int> out_shape_;
+    };
+
+}
+
+#endif //LOADPARAM_PRELU_LAYER_HPP
---
+++ src/layers/prelu_layer.cpp
@@ -0,0 +1,62 @@
+//
+// Created by liukai on 19-4-4.
+//
+#include <vector>
+#include "layers/prelu_layer.hpp"
+
+using namespace std;
+
+namespace caffe{
+
+    void PReluLayer::SetUp(const LayerParameter& param, const vector<Blob<double>* >& bottom, vector<Blob<double>* >& top)
+    {
+        cout << "PReluLayer::SetUp()" << param.name() << endl;
+        CHECK_EQ(bottom.size(), 1)<<"Bottom size for convolution layer must be 1"<<endl;
+        CHECK_EQ(top.size(), 1)<<"Top size for convolution layer must be 1"<<endl;
+
+//        layer_name_ = param.name();
+//        layer_type_ = param.type();
+//        for(int bottom_id=0; bottom_id<param.bottom_size(); ++bottom_id)
+//        {
+//            bottom_names_.push_back(param.bottom(bottom_id));
+//        }
+//        for(int top_id=0; top_id<param.top_size(); ++top_id)
+//        {
+//            top_names_.push_back(param.top(top_id));
+//        }
+
+        in_shape_ = bottom[0]->shape();
+        vector<int> weight_shape{1,in_shape_[1],1,1};
+        weights()[0].reset(new Blob<double>(weight_shape));
+//        out_shape_ = bottom[0]->shape();
+        top[0]->Reshape(in_shape_);
+    }
+
+
+    void PReluLayer::Forward(const vector<Blob<double>* >& bottom, vector<Blob<double>* >& top)
+    {
+        cout << "PReluLayer::forward()..." << endl;
+        int N = in_shape_[0];
+        int C = in_shape_[1];
+        int Hx = in_shape_[2];
+        int Wx = in_shape_[3];
+        for (int n = 0; n < N; ++n)
+        {
+            for (int c = 0; c < C; ++c)
+            {
+                double negSlope=(*weights()[0])(0,c,0,0);
+                for (int w = 0; w < Wx; ++w)
+                {
+                    for (int h = 0; h < Hx; ++h)
+                    {
+                        double tmp=(*bottom[0])(n,c,h,w);
+                        (*top[0]).at(n,c,h,w) = tmp>0 ? tmp : negSlope*tmp;
+                    }
+                }
+            }
+        }
+
+
+    }
+
+}
